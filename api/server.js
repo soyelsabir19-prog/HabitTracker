a94d23c57c33394165a1f394d292f355da3b81a3
agni270 <lokagni720@gmail.com>
2026-02-17 21:11:49 +0530

    fixed vercel server handler

--- api/server.js
+++ api/server.js
@@ -1,19 +1,15 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Test route
 app.get("/", (req, res) => {
-  res.json({ message: "Main API working" });
+  res.status(200).json({ message: "Main API working" });
 });
 
-// TODO: We will paste your real routes here next
-
-export default app;
+export default function handler(req, res) {
+  return app(req, res);
+}
